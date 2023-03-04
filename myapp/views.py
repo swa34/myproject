e52429c127757c7d7dde37360c2075b28730c2c0
Scott Allen <71349891+swa34@users.noreply.github.com>
2023-03-03 23:05:24 -0500

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import openai
+
+openai.api_key = ""
+
+
+def index(request):
+    if request.method == 'POST':
+        question = request.POST['question']
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=question,
+            temperature=0.5,
+            max_tokens=60,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        return render(request, 'response.html', {'response': response.choices[0].text})
+    else:
+        return render(request, 'index.html')
---
+++ myproject/__init__.py
@@ -0,0 +1,2 @@
+from myapp import views
+# Empty __init__.py file
